604c7145f406e3d176577d5e2b734261eb1c271c
amrxt <amrxt11@outlook.com>
2024-01-02 21:51:46 -0800

    Users able to create their posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,25 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+
+  def new
+    @post = current_user.posts.build
+    puts "New Post by #{current_user.name}"
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      redirect_to root_path, alert: "Post saved"
+    else
+      render "new", alert: "Post was not saved"
+    end
+  end
+
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
 end
