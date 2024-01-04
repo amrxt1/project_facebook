929da6a510b2c1b5c7ac5281cc58654f49fe0ecb
amrxt <amrxt11@outlook.com>
2024-01-03 18:06:43 -0800

    Set up Like model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,9 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, presence: true
   belongs_to :user
+
+  has_many :likes
+  has_many :liked_by, through: :likes, source: :user
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :posts
   validates :name, presence: true
+
+  has_many :likes, dependent: :destroy
+  has_many :liked_posts, through: :likes, source: :post
 end
