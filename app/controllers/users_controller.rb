f13e9619f283b537a3ba8ecbbacf19a43d8aed29
amrxt <amrxt11@outlook.com>
2024-02-06 18:14:12 -0800

    Change styles and fix relationship error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,14 @@
 class UsersController < ApplicationController
   def following
-    @following = User.find(params[:id]).following
+    @user = User.find(params[:id])
+    @following = @user.following
     render 'following'
   end
 
   def show
     @user = User.find(params[:id])
+    puts @user.name
+    puts @user.followers
     @posts = @user.posts
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,16 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :posts
+  has_many :posts, dependent: :destroy
   validates :name, presence: true
 
   has_many :likes, dependent: :destroy
-  has_many :liked_posts, through: :likes, source: :post
-  has_many :comments, as: :commentable
+  has_many :liked_posts, through: :likes, source: :post, dependent: :destroy
+  has_many :comments, as: :commentable, dependent: :destroy
 
   has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
   has_many :following, through: :active_relationships, source: :followed
 
   has_many :passive_relationships, class_name:  "Relationship", foreign_key: "followed_id", dependent:   :destroy
-  has_many :followers, through: :active_relationships, source: :follower
+  has_many :followers, through: :passive_relationships, source: :follower
 end
