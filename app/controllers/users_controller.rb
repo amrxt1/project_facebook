d4f02e52af2633746d74f44a6be0acfa62437700
amrxt <amrxt11@outlook.com>
2024-02-18 16:08:18 -0800

    Create Users#index page and controller action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,17 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+    puts @user.name
+    puts @user.followers
+    @posts = @user.posts
+    @show_followers = true
+  end
+
+  def index
+    @users = User.all.order(created_at: :desc)
+  end
+
+  #Custom Routes
   def following
     @user = User.find(params[:id])
     @following = @user.following
@@ -9,14 +22,6 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @followers = @user.followers
     render 'followers'
-  end
-
-  def show
-    @user = User.find(params[:id])
-    puts @user.name
-    puts @user.followers
-    @posts = @user.posts
-    @show_followers = true
   end
 
   def my_profile
